6f209477a64ed8ef4099cf62cf9b581bedb9f179
princeGannett <pverma@gannett.com>
2018-07-29 14:01:33 +0530

    updated server url.

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 export default {
   SERVER_NATIVE_URL: "http://139.59.36.219:80",
+  // SERVER_NATIVE_URL: "http://172.20.10.23:8000",
   SERVER_SOCKET_URL: "http://localhost:7900",
   USERNAME_REGEX: /^[A-Za-z0-9.]+(?:[_$-][A-Za-z0-9.]+)*$/,
   API_TIME_OUT : 60*1000
